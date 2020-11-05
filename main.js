b7222c5df546d1e13cb9ac5f2b2a3293a51e934f
samsularifin05 <samsul026@gmail.com>
2020-11-05 11:47:59 +0700

    fix server

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const wa = require('@open-wa/wa-automate');
 const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
+const winston = require("winston");
  
 const serverOption = {
 	headless: true,
@@ -34,31 +35,33 @@ wa.create(serverOption).then(async client => await start(client))
 const PORT = process.env.PORT;
 
 const start = async (client) => {
-	const app = express();
-	app.use(bodyParser.urlencoded({ extended: true }));
-	app.use(bodyParser.json());
-	app.use(bodyParser.raw());
-	app.get('/', (req, res) => res.send('Hello'));
-		app.post('/kirim-pesan', (req, res) => {
-	client.sendText(req.body.no_hp + '@c.us', req.body.pesan);
-	//send images
-	// client.sendImage(req.body.no_hp + '@c.us', req.body.gambar);
-	//send video / foto
-	// client.sendFileFromUrl(req.body.no_hp + '@c.us','https://i.giphy.com/media/oYtVHSxngR3lC/200w.mp4','nama_file');
-	// send file pdf /doc
-	// client.sendFile(
+  const app = express();
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
+  app.use(bodyParser.raw());
+  app.get("/", (req, res) => res.send("Hello"));
+  app.post("/kirim-pesan", (req, res) => {
+    client.sendText(req.body.no_hp + "@c.us", req.body.pesan);
+    //send images
+    // client.sendImage(req.body.no_hp + '@c.us', req.body.gambar);
+    //send video / foto
+    // client.sendFileFromUrl(req.body.no_hp + '@c.us','https://i.giphy.com/media/oYtVHSxngR3lC/200w.mp4','nama_file');
+    // send file pdf /doc
+    // client.sendFile(
     //     req.body.no_hp + "@c.us",
     //     "https://www.lyfemarketing.com/blog/wp-content/uploads/2017/12/Digital-Marketing-Strategy-eBook.pdf",
     //     "nama_file"
     //   );
-      // client.sendYoutubeLink(req.body.no_hp + '@c.us','https://www.youtube.com/watch?v=_bSB6Ed2Fnk','Ytube');
-      res.send(["berhasil"]);
-    });
-	  
-	app.listen(4000, () => console.log('listenig on localhost:4000'));
-  	client.onMessage((message) => {
-    	wa.msgHandler(client, message, mqttClient)
-  	})
+    // client.sendYoutubeLink(req.body.no_hp + '@c.us','https://www.youtube.com/watch?v=_bSB6Ed2Fnk','Ytube');
+    res.send(["berhasil"]);
+  });
+
+  // app.listen(4000, () => console.log('listenig on localhost:4000'));
+  const port = 4000 || 3753;
+  app.listen(port, () => winston.info(`Listening on port ${port}...`));
+  client.onMessage((message) => {
+    wa.msgHandler(client, message, mqttClient);
+  });
 }
  
 
